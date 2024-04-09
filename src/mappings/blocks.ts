fca3acfd41e6878f1151d31f7ad6f1bba6f1f0ad
yinwei <82604458@qq.com>
2024-04-09 16:14:02 +0800

    update

--- src/mappings/blocks.ts
+++ src/mappings/blocks.ts
@@ -1,4 +1,5 @@
 import {
+    BigInt,
     ethereum
 } from "@graphprotocol/graph-ts"
 
@@ -10,17 +11,22 @@ export function handleBlock(block: ethereum.Block): void {
     let id = block.hash.toHex()
     let blockEntity = new Block(id);
     blockEntity.number = block.number;
-    // blockEntity.timestamp = block.timestamp;
-    // blockEntity.parentHash = block.parentHash.toHex();
-    // blockEntity.author = block.author.toHex();
-    // blockEntity.difficulty = block.difficulty;
-    // blockEntity.totalDifficulty = block.totalDifficulty;
-    // blockEntity.gasUsed = block.gasUsed;
-    // blockEntity.gasLimit = block.gasLimit;
-    // blockEntity.receiptsRoot = block.receiptsRoot.toHex();
-    // blockEntity.transactionsRoot = block.transactionsRoot.toHex();
-    // blockEntity.stateRoot = block.stateRoot.toHex();
-    // blockEntity.size = block.size;
-    // blockEntity.unclesHash = block.unclesHash.toHex();
+    blockEntity.timestamp = block.timestamp;
+    if(block.number == BigInt.fromI32(0)){
+        blockEntity.parentHash = "0x0";
+        blockEntity.stateRoot = "0x0";
+    } else{
+        blockEntity.parentHash = block.parentHash.toHex();
+        blockEntity.stateRoot = block.stateRoot.toHex();
+    }
+    blockEntity.author = block.author.toHex();
+    blockEntity.difficulty = block.difficulty;
+    blockEntity.totalDifficulty = block.totalDifficulty;
+    blockEntity.gasUsed = block.gasUsed;
+    blockEntity.gasLimit = block.gasLimit;
+    blockEntity.receiptsRoot = block.receiptsRoot.toHex();
+    blockEntity.transactionsRoot = block.transactionsRoot.toHex();
+    blockEntity.size = block.size;
+    blockEntity.unclesHash = block.unclesHash.toHex();
     blockEntity.save();
   }
